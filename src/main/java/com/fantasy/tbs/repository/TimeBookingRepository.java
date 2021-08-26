092a8906bc63bfc3ccb05357b5cfe136954c7fcc
Binsirius <phoenix.shen1@gmail.com>
2021-08-26 19:58:31 +0800

    Initial commit

---
+++ src/main/java/com/fantasy/tbs/repository/TimeBookingRepository.java
@@ -0,0 +1,16 @@
+package com.fantasy.tbs.repository;
+
+import com.fantasy.tbs.domain.TimeBooking;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Spring Data SQL repository for the TimeBooking entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TimeBookingRepository extends JpaRepository<TimeBooking, Long> {
+    List<TimeBooking> findByPersonalNumber(String personalName);
+}
